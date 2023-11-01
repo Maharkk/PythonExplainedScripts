b5210cf512aedbc6d1fbe8f9122f2bb3db9422b2
Maharkk <maharkk01@gmail.com>
2023-11-01 09:18:31 +0530

    Updated Lists Scripts added

--- Lists.py
+++ Lists.py
@@ -1,29 +1,29 @@
 #list
-a = [1,2,3,4,5,6] # This is a list
-a[3] = 88  # This will change the 4 to 88 and we can change it because list is mutable object
-print(a) 
-print(a[2]) # Accessing list using index numbers
+a = [1, 2, 3, 4, 5, 6]  # This is a list
+a[3] = 88  # Changes the value at index 3 (4th element) to 88
+print(a)  # Output: [1, 2, 3, 88, 5, 6]
+print(a[2])  # Output: 3 (Accesses the value at index 2)
 #print(a[6]) # This wont work because IndexError: list index out of range
 
-# We can include different datatypes in list
-b = [2,"mahar",True,7.1]
-print(b)
+b = [2, "mahar", True, 7.1]  # A list can include different data types
+print(b)  # Output: [2, 'mahar', True, 7.1]
 
 # List slicing
 friends = ["Mahar", "Surya", "Nuhman", "Safwan", "Aslam", 71]
-print(friends[0:3]) # This slicing starts from 0 to 3 and will 0 to 2 of list
-print(friends[-3:]) # This slicing starts from -4 and print till last of list
+print(friends[0:3])  # Output: ['Mahar', 'Surya', 'Nuhman'] (Slices elements at index 0 to index 2)
+print(friends[-3:])  # Output: ['Safwan', 'Aslam', 71] (Slices last 3 elements)
 
 # List methods
-l1 = [1,8,7,2,21,15]
-print(l1) # as you can see the list not sorted
-l1.sort() # sort function will sort the list
-print(l1) # now the updated sorted list will print
-#l1.reverse() # This will reverse the list
-#l1.append(10) # This function will adds to the end of the list
-#l1.insert(2,44) # This will insert 44 at index 2
-#l1.pop(2) # This will delete the value at index 2
-#l1.remove(21) # This will remove 21 from the list
+l1 = [1, 8, 7, 2, 21, 15]
+print(l1)  # Output: [1, 8, 7, 2, 21, 15]
+l1.sort()  # Sorts the list
+print(l1)  # Output: [1, 2, 7, 8, 15, 21] (Sorted list)
+# Uncomment and test the following methods one at a time to see their effects:
+# l1.reverse()  # Reverses the list
+# l1.append(10)  # Adds 10 to the end of the list
+# l1.insert(2, 44)  # Inserts 44 at index 2
+# l1.pop(2)  # Removes the value at index 2
+# l1.remove(21)  # Removes the first occurrence of value 21 from the list
 
 # You can get all the list methods from "python docs" website
 
